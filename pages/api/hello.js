cfb379cff55cccd142872fba30b6abef09171e0d
zackenichi <thekenichi26@gmail.com>
2023-07-06 20:35:41 +0800

    [feature][zac] test openai connection

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,30 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Configuration, OpenAIApi } from 'openai';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const numberOfChoices = 3;
+
+export default async function handler(req, res) {
+  try {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: 'hello ChatGPT',
+      temperature: 0.6,
+      max_tokens: 100,
+      n: numberOfChoices,
+    });
+
+    const response = completion.data.choices;
+
+    const chats = response.map(
+      (choice, index) => `Choice ${index + 1}: ${choice.text}`
+    );
+    res.status(200).json(chats);
+  } catch (error) {
+    console.error('Error with OpenAI API request:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
