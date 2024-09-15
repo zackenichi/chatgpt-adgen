bbe0c3b77519aa688eeeae1221dc738566df627e
ZacKenichi <>
2024-09-15 19:21:53 +0800

    update-access

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,22 +5,30 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+// console.log('API Key:', process.env.OPENAI_API_KEY);
+
 const numberOfChoices = 3;
 
 export default async function handler(req, res) {
   try {
-    const completion = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt: 'hello ChatGPT',
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          content:
+            'hello ChatGPT, do you have any suggestions for me to do today?',
+        },
+      ], // This is the correct way to format chat input
       temperature: 0.6,
-      max_tokens: 100,
       n: numberOfChoices,
+      max_tokens: 100,
     });
 
     const response = completion.data.choices;
 
     const chats = response.map(
-      (choice, index) => `Choice ${index + 1}: ${choice.text}`
+      (choice, index) => `Choice ${index + 1}: ${choice.message.content}`
     );
     res.status(200).json(chats);
   } catch (error) {
